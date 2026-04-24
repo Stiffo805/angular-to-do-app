c5a20b4422922efcb7623a210bd65739bb997542
Adam Konior <akonior632@gmail.com>
2026-04-24 17:20:10 +0200

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoList } from './todo-list/todo-list';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroCheck, heroMinus, heroPlus } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, TodoList, NgIcon],
+  template: ` <div class="flex min-h-screen items-center justify-center">
+    <app-todo-list />
+  </div>`,
+  styleUrl: './app.css',
+  viewProviders: [provideIcons({ heroMinus, heroCheck, heroPlus })]
 })
 export class App {
   protected readonly title = signal('frontend');
